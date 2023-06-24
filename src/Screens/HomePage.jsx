fc37b1d426cace220b49a27a2f7a2310eeb74573
Bruno Alpachino <brunolalpachino@gmail.com>
2023-06-24 11:02:36 +0300

    used json fake api to make call to a fake api

--- src/Screens/HomePage.jsx
+++ src/Screens/HomePage.jsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomePage = () => {
   const [todos, setTodos] = useState([]);
 
-  useEffect(() => {}, []);
-  const getTodos = async () => {};
+  useEffect(() => {
+    const getTodos = async () => {
+      const data = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      console.log(data);
+
+      console.log("getTodos");
+    };
+    getTodos();
+  }, []);
+
   return <div>HomePage</div>;
 };
 
