7cb6914478f034fd8a9586541e102bebd619d0d5
Bruno Alpachino <brunolalpachino@gmail.com>
2023-06-24 11:17:22 +0300

    modified the functions fetching form the api

--- src/Screens/HomePage.jsx
+++ src/Screens/HomePage.jsx
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const HomePage = () => {
-  const [todos, setTodos] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    const getTodos = async () => {
-      const data = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      console.log(data);
-
-      console.log("getTodos");
-    };
-    getTodos();
+    getUsers();
   }, []);
 
-  return <div>HomePage</div>;
+  const getUsers = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    console.log(response);
+    // const users = await response.json();
+    setUsers(response.data);
+  };
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li>{user.name}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default HomePage;
